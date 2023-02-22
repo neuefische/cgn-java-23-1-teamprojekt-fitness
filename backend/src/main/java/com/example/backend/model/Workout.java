f5038169b6bc8f2d7e24f8877488233d0fd1bb1e
alaajouri <alaa.joryi@hotmail.com>
2023-02-22 16:18:31 +0100

    Add frontend and backend Packages and files

---
+++ backend/src/main/java/com/example/backend/model/Workout.java
@@ -0,0 +1,10 @@
+package com.example.backend.model;
+
+public record Workout(
+        String description ,
+        String id ,
+        String title
+
+) {
+
+}
---
+++ backend/src/main/java/com/example/backend/service/WorkoutService.java
@@ -0,0 +1,12 @@
+package com.example.backend.service;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import com.example.backend.model.Workout;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+@Repository
+@RequiredArgsConstructor
+public class WorkoutService {
+}
