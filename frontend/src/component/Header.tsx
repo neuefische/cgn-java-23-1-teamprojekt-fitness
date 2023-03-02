1cd21359bf88a5c0c374cbcdd8c2547c63784749
alaajouri <alaa.joryi@hotmail.com>
2023-03-02 16:59:05 +0100

    Changes by rohkazimi

--- frontend/src/component/Header.tsx
+++ frontend/src/component/Header.tsx
@@ -7,8 +7,10 @@ export default function Header() {
             <h1>Fitness App</h1>
             <img
                 src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJggdsCjDB8RsGtl-8yt1epSsI36o-LuZoJw&usqp=CAU"}/>
-            <button className={"button-header"}><Link className={"link-header"} to={"/workouts/add"}>add Workout</Link> </button>
-            <button className={"button-header"}><Link className={"link-header"} to={"/"}>Go back to Homepage</Link> </button>
+            <button className={"button-header"}><Link className={"link-header"} to={"/workouts/add"}>Add Workout</Link>
+            </button>
+            <button className={"button-header"}><Link className={"link-header"} to={"/"}>Go back to Homepage</Link>
+            </button>
         </header>
     );
 }
