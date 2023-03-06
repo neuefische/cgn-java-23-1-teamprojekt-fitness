c57764f18a07581898e221e16e44ff5ead4d8d34
Roh Kazimi <rohula-kazimi@hotmail.com>
2023-03-06 17:58:20 +0100

    add UserController tests

---
+++ backend/src/test/java/com/example/backend/controller/MongoUserControllerTest.java
@@ -0,0 +1,32 @@
+package com.example.backend.controller;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class MongoUserControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+
+    @Test
+    @DirtiesContext
+    @WithMockUser(username = "user", password = "password")
+    void getMe_whenAuthenticated_thenUsername() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/users/me"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("user"));
+    }
+
+
+
+}
