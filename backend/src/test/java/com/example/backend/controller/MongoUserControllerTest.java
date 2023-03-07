12742f80bf4f9c14380a4caba51b357c7296953f
Roh Kazimi <rohula-kazimi@hotmail.com>
2023-03-07 10:26:19 +0100

    add csrf securityConfig and test

--- backend/src/test/java/com/example/backend/controller/MongoUserControllerTest.java
+++ backend/src/test/java/com/example/backend/controller/MongoUserControllerTest.java
@@ -1,13 +1,16 @@
 package com.example.backend.controller;
+
 import org.junit.jupiter.api.Test;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -16,15 +19,31 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class MongoUserControllerTest {
     @Autowired
     MockMvc mockMvc;
+    @WithMockUser(username = "user", password = "password")
 
 
     @Test
     @DirtiesContext
-    @WithMockUser(username = "user", password = "password")
+
     void getMe_whenAuthenticated_thenUsername() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/users/me"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/user/me"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("user"));
+    }
+
+    @Test
+    @DirtiesContext
+    void create_whenValid_then200() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "username": "test",
+                                    "password": "test"
+                                }
+                                """)
+                        .with(csrf()))
+                .andExpect(status().isOk());
     }
 
 
