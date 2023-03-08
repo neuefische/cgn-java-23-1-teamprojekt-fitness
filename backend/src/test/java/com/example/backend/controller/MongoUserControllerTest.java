e11d3addc4f5e2212d9c4f4d6d57a5dcbf19283d
Roh Kazimi <rohula-kazimi@hotmail.com>
2023-03-08 15:57:52 +0100

    fix tests

--- backend/src/test/java/com/example/backend/controller/MongoUserControllerTest.java
+++ backend/src/test/java/com/example/backend/controller/MongoUserControllerTest.java
@@ -11,7 +11,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -19,17 +18,26 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class MongoUserControllerTest {
     @Autowired
     MockMvc mockMvc;
-    @WithMockUser(username = "user", password = "password")
+
 
 
     @Test
     @DirtiesContext
-
+    @WithMockUser(username = "user", password = "password")
     void getMe_whenAuthenticated_thenUsername() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/user/me"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("user"));
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/user/me").with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                            "username": "user",
+                            "password":"123"
+                        }
+                        """));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/users/me"))
+                .andExpect(status().isOk());
     }
+
 
     @Test
     @DirtiesContext
--- backend/src/test/java/com/example/backend/controller/WorkoutControllerTest.java
+++ backend/src/test/java/com/example/backend/controller/WorkoutControllerTest.java
@@ -3,15 +3,17 @@ package com.example.backend.controller;
 import com.example.backend.model.Workout;
 import com.example.backend.repo.WorkoutRepo;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.junit.jupiter.api.Test;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
@@ -34,21 +36,23 @@ class WorkoutControllerTest {
 
     @Test
     @DirtiesContext
+    @WithMockUser(username = "user", password = "password")
     void checkListAllWorkouts() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/workouts"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/workouts").with(csrf()))
                 .andExpect(status().isOk())
                 .andExpect(content().json("[]"));
     }
 
     @Test
     @DirtiesContext
+    @WithMockUser(username = "user", password = "password")
     void getWorkoutById() throws Exception {
         // GIVEN
         workoutRepo.save(workout1);
         workoutRepo.save(workout2);
 
         // WHEN
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/workouts/" + workout1.id()))
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/workouts/" + workout1.id()).with(csrf()))
                 .andExpect(status().isOk())
                 .andExpect(content().json("""
                         {                    
@@ -60,19 +64,21 @@ class WorkoutControllerTest {
 
     @Test
     @DirtiesContext
+    @WithMockUser(username = "user", password = "password")
     void deleteWorkout() throws Exception {
         // GIVEN
         workoutRepo.save(workout1);
 
         // WHEN
-        mockMvc.perform(MockMvcRequestBuilders.delete("/api/workouts/" + workout1.id()))
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/workouts/" + workout1.id()).with(csrf()))
                 .andExpect(status().isOk());
     }
 
     @Test
     @DirtiesContext
+    @WithMockUser(username = "user", password = "password")
     void checkAddWorkout() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/workouts").contentType(MediaType.APPLICATION_JSON)
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/workouts").with(csrf()).contentType(MediaType.APPLICATION_JSON)
                         .content("""
                                 {                     
                                 "description": "Mit dem Fahrrad fahren durch den Wald",
