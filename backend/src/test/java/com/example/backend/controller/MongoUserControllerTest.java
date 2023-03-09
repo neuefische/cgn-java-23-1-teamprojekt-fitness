d2ea026c84368e3cbf3d0264773b762230dc89e9
Roh Kazimi <rohula-kazimi@hotmail.com>
2023-03-09 16:51:46 +0100

    add remaining userTests

--- backend/src/test/java/com/example/backend/controller/MongoUserControllerTest.java
+++ backend/src/test/java/com/example/backend/controller/MongoUserControllerTest.java
@@ -1,5 +1,7 @@
 package com.example.backend.controller;
 
+import com.example.backend.model.MongoUser;
+import com.example.backend.repo.MongoUserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,7 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
