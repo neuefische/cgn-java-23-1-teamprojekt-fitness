605ea46ede979a9d928542d69132adabbd2468f8
Roh Kazimi <rohula-kazimi@hotmail.com>
2023-03-10 10:39:45 +0100

    Changes by alaajouri

--- frontend/src/component/SignInPage.tsx
+++ frontend/src/component/SignInPage.tsx
@@ -2,57 +2,42 @@ import React from "react";
 import {useNavigate} from "react-router-dom";
 import axios from "axios";
 import Layout from "./Layout";
+import AuthForm from "../component/AuthForm";
 import useAuth from "../hooks/useAuth";
 
 export default function SignInPage () {
-
-    const {username,password,setUsername,setPassword} =useAuth(true) ;
     const navigate = useNavigate();
+    const { setUsername, setPassword, username, password } = useAuth(true);
+
+    const handleSignIn = (username: string, password: string) => {
+        axios
+            .post(
+                "/api/user/login",
+                {},
+                {
+                    headers: {
+                        Authorization: `Basic ${window.btoa(`${username}:${password}`)}`,
+                    },
+                }
+            )
+            .then(() => {
+                const redirect =
+                    window.sessionStorage.getItem("signInRedirect") || "/";
+                window.sessionStorage.removeItem("signInRedirect");
+                navigate(redirect);
+            })
+            .catch((err) => {
+                alert(err.response.data.error);
+            });
+    };
 
     return (
         <Layout>
-        <div style={{padding: "5rem 0"}}>
-            <h1>Sign In</h1>
-
-            <form onSubmit={e => {
-                e.preventDefault();
-                axios.post("/api/user/login", {}, {
-                    headers: {
-                        Authorization: `Basic ${window.btoa(`${username}:${password}`)}`
-                    }
-                }).then(() => {
-                    const redirect = window.sessionStorage.getItem("signInRedirect") || "/";
-                    window.sessionStorage.removeItem("signInRedirect");
-                    navigate(redirect);
-                }).catch(err => {
-                    alert(err.response.data.error);
-                });
-            }}>
-                <div>
-                    <label>
-                        Username
-                        <input
-                            type="text"
-                            value={username}
-                            onChange={e => setUsername(e.currentTarget.value)}
-                        />
-                    </label>
-                </div>
-
-                <div>
-                    <label>
-                        Password
-                        <input
-                            type="password"
-                            value={password}
-                            onChange={e => setPassword(e.currentTarget.value)}
-                        />
-                    </label>
-                </div>
-
-                <button type="submit">Sign In</button>
-            </form>
-        </div>
+            <AuthForm
+                title="Sign In"
+                buttonText="Sign In"
+                onSubmit={handleSignIn}
+            />
         </Layout>
     );
 }
--- frontend/src/component/SignUpPage.tsx
+++ frontend/src/component/SignUpPage.tsx
@@ -4,55 +4,32 @@ import axios from "axios";
 import {useNavigate} from "react-router-dom";
 import Layout from "./Layout";
 import useAuth from "../hooks/useAuth";
+import AuthForm from "../component/AuthForm";
 
 
 
 export default function SignUpPage () {
-    const {username,password,setUsername,setPassword} =useAuth(true) ;
     const navigate = useNavigate();
+    const { setUsername, setPassword, username, password } = useAuth(true);
+
+    const handleSignUp = (username: string, password: string) => {
+        axios
+            .post("/api/user", { username, password })
+            .then(() => {
+                navigate("/sign-in");
+            })
+            .catch((err) => {
+                alert(err.response.data.error);
+            });
+    };
 
     return (
         <Layout>
-        <div style={{padding: "5rem 0"}}>
-
-            <h1>Sign In</h1>
-
-            <form onSubmit={e => {
-                e.preventDefault();
-                axios.post("/api/user", {
-                    username,
-                    password
-                }).then(() => {
-                    navigate("/sign-in");
-                }).catch(err => {
-                    alert(err.response.data.error);
-                });
-            }}>
-                <div>
-                    <label>
-                        Username
-                        <input
-                            type="text"
-                            value={username}
-                            onChange={e => setUsername(e.currentTarget.value)}
-                        />
-                    </label>
-                </div>
-
-                <div>
-                    <label>
-                        Password
-                        <input
-                            type="password"
-                            value={password}
-                            onChange={e => setPassword(e.currentTarget.value)}
-                        />
-                    </label>
-                </div>
-
-                <button type="submit">Sign Up</button>
-            </form>
-        </div>
+            <AuthForm
+                title="Sign Up"
+                buttonText="Sign Up"
+                onSubmit={handleSignUp}
+            />
         </Layout>
     );
 }
