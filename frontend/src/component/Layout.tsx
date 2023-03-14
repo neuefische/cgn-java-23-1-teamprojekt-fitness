4c16fbf5ae98b6944db57afdab63935661d5d52c
Roh Kazimi <rohula-kazimi@hotmail.com>
2023-03-14 17:57:22 +0100

    Changes by lenaruppert

--- frontend/src/component/Layout.tsx
+++ frontend/src/component/Layout.tsx
@@ -1,6 +1,7 @@
 import React, {ReactNode} from "react";
 import {Link, useLocation} from "react-router-dom";
 import LogOut from "./Logout";
+import "./Layout.css";
 
 type Props = {
     children: ReactNode;
@@ -10,7 +11,7 @@ export default function Layout ({children}: Props) {
     const location = useLocation();
 
     return (
-        <div>
+        <div className={"Layout"}>
             {!["/sign-in", "/sign-up"].includes(location.pathname) && (
                 <div>
                     <Link to={"/sign-up"}>Sign Up</Link> &nbsp; <Link to={"/sign-in"}>Sign In</Link> &nbsp; <LogOut/>
